15086389d5b8c02c18b66b819246889f5186c8c3
Shoddiq Jati Premono <shoddiq.jati@gmail.com>
2019-09-20 10:53:06 +0700

    add wa integration

--- app/src/main/java/com/jati/dev/androidfcm/MainActivity.kt
+++ app/src/main/java/com/jati/dev/androidfcm/MainActivity.kt
@@ -1,27 +1,41 @@
 package com.jati.dev.androidfcm
 
-import android.support.v7.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.google.firebase.iid.FirebaseInstanceId
-import com.google.firebase.messaging.FirebaseMessaging
 import com.jati.dev.androidfcm.utils.VersionChecker
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity(), VersionChecker.OnUpdateNeededListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkAppVersion()
 
         VersionChecker.with(this).onUpdateNeeded(this).check()
-        FirebaseMessaging.getInstance().subscribeToTopic("all")
-    }
+        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener { task ->
+            task.result?.token.let { token.text = it }
+        }
 
-    private fun checkAppVersion() {
-        tvVersion.text = packageManager.getPackageInfo(packageName, 0).versionName
+        send.setOnClickListener {
+            if (phone.text.isNotEmpty()) sendToWhatsApp()
+        }
+
+        token.setOnLongClickListener {
+            val cm = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clipData = ClipData.newPlainText("token", token.text.toString())
+            cm.primaryClip = clipData
+            Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+            true
+        }
     }
 
     private fun snackbar(message: String) {
@@ -30,5 +44,12 @@ class MainActivity : AppCompatActivity(), VersionChecker.OnUpdateNeededListener 
 
     override fun onUpdateNeeded(updateUrl: String) {
         snackbar(updateUrl)
+    }
+
+    private fun sendToWhatsApp() {
+        startActivity(Intent(Intent.ACTION_VIEW).apply {
+            data =
+                Uri.parse("http://api.whatsapp.com/send?phone=${phone.text}&text=${token.text.toString()}")
+        })
     }
 }
